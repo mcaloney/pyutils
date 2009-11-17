61caa017cf7088abda3b5062bc75f76770b5be56
Chris McAloney <mcaloney@ivory.(none)>
2009-11-17 08:32:44 -0400

    dupe: incremental improvements on the dupefinder

--- dupefinder.py
+++ dupefinder.py
@@ -21,57 +21,33 @@ def print_files_and_md5(arg, directory, files):
             m.update(open(fullpath).read())
             print fullpath, m.hexdigest()
 
-def split_duplicates(items, filter_fn):
-    originals = []
-    duplicates = []
-    for duplicate_list in items:
-        orig, dupe = filter_fn(duplicate_list)
-        originals.append(orig)
-        duplicates.extend(dupe)
-    return originals, duplicates
-
 def print_all_files(arg, directory, files):
     for filename in files:
         print os.path.join(directory, filename)
 
+def dump_to_file(data, filename):
+    output_file = open(filename, 'w')
+    output_file.write(repr(data))
+    output_file.close()
+
 if __name__ == '__main__':
     hashes = {}
     # walk through the directory structure, hashing the files in each subdir
-    os.path.walk(os.path.expandvars('$HOME/Desktop/test_docs'), hash_directory, hashes)
+#    os.path.walk(os.path.expandvars('$HOME/Desktop/test_docs'), hash_directory, hashes)
+#    # dump the hashes to a separate file so I don't have to keep regenerating them
+#    # while testing this
+#    dump_to_file(hashes, '/Users/mcaloney/src/pyutils/hash_values')
 
-    # in a separate processing step, need to iterate through the hashes
-    # determining which of the duplicate files is the "original".
-    # Since all files have a common base directory, the "original":
-    #   - has the shallowest directory structure, and
-    #   - the earliest creation date
+    # while testing, use our cached hashes list
+    hashes = eval(open('/Users/mcaloney/src/pyutils/hash_values', 'r').read())
 
-    def file_list_cmp(x, y):
-        # compare the filenames by number of path components, then ctime
-        x_path_len = x.count(os.path.sep)
-        y_path_len = y.count(os.path.sep)
-        if x_path_len != y_path_len:
-            return cmp(x_path_len, y_path_len)
-        return cmp(os.path.getctime(x), os.path.getctime(y))
-
-    def file_list_filter(filenames):
-        if len(filenames) == 0:
-            raise IndexError
-        # sort the filenames by our ranking criteria
-        filenames.sort(file_list_cmp)
-        return filenames[0], filenames[1:]
-
-    originals, duplicates = split_duplicates(hashes.values(), file_list_filter)
-
-    # dump the hashes to a separate file so I don't have to keep regenerating them
-    # while testing this
-    output_file = open(os.path.expandvars('$HOME/Desktop/test_docs/hash_values'), 'w')
-    output_file.write(repr(hashes))
-    output_file.close()
-
-    # sanity testing
-#    print 'Finished gathering duplicates; checking ...'
-#    for k,v in hashes.items():
-#        if len(v) > 1:
-#           filenames = map(os.path.basename, v)
-#           if (len(set(filenames)) > 1):
-#               print 'COLLISION:', ','.join(filenames)
+    # sort the files; pick a representative from each equiv class and
+    # put it in an 'originals' dir with its hashcode as its filename
+    # the rest go in hashcode-distinguished subdirs of 'duplicates'
+    # so that they can be "easily" verified.
+    output_dir = '/Users/mcaloney/images'
+    duplicates_dir_name = 'duplicates'
+    originals_dir_name = 'originals'
+    duplicates_dir = os.path.join(output_dir, duplicates_dir_name)
+    for hash, files in hashes.items():
+        original_name = hash + splitext(basename(files[0]))[1]
